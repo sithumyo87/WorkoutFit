77953edf29a7794f4857fafb79ac10791bee8401
Si Thu Myo <sithumyo87@gmail.com>
2022-11-08 00:43:58 +0630

    Gym Stage

---
+++ resources/views/Frontend/diet_detail.blade.php
@@ -0,0 +1,205 @@
+@extends('template')
+@section('title','Home')
+@section('content')
+<!-- Blog Details Hero Section Begin -->
+<section class="blog-details-hero set-bg" data-setbg="img/blog/details/details-hero.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 p-0 m-auto">
+                <div class="bh-text">
+                    <h3>Workout nutrition explained. What to eat before, during, and after exercise.</h3>
+                    <ul>
+                        <li>by Admin</li>
+                        <li>Aug,15, 2019</li>
+                        <li>20 Comment</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Blog Details Hero Section End -->
+
+<!-- Blog Details Section Begin -->
+<section class="blog-details-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 p-0 m-auto">
+                <div class="blog-details-text">
+                    <div class="blog-details-title">
+                        <h5>Calories in Food: Calorie Chart Database</h5>
+                        <p>Preserved fruit that has been sealed in a can or airtight jar often has a higher amount of
+                            sugar and thus more calories as compared to fresh fruit. However, it is possible for canned
+                            fruit to have the same calories and nutritional information as its newly harvested
+                            counterpart. When picked and preserved during the peak period, canned fruit may even retain
+                            a higher percentage of nutrients. All fruits are mostly made of carbohydrates, although
+                            calories in canned fruit can also come from fats and small amounts of protein. Fruits are
+                            all said to have good nutritional value since they contain a lot of vitamins and minerals
+                            per serving, including potassium, phosphorous, and vitamins A and C. Most varieties of
+                            fruits that are canned have a naturally high amount of sugar. For example, peaches, pears,
+                            and papaya each contain over 40 grams of sugar per 100 grams. If excess sugar has been added
+                            during the fruit canning process, that is typically evident by a high number of calories as
+                            indicated in the nutrition facts. The calorie chart below can be used as a guideline, but
+                            exact amounts for each brand will differ depending on the amount of syrup used, if any.
+                        </p>
+                    </div>
+                    
+
+                    <table class="table">
+                        <thead>
+                            <tr class="table-color">
+                                <th>Food</th>
+                                <th>Serving</th>
+                                <th>Calories</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="table-dark ">
+                                <th scope="row">Applesauce</th>
+                                <td>1 cup (114 g)	</td>
+                                <td>71 cal</td>
+                            </tr>
+                            <tr class="table-dark ">
+                                <th scope="row">juice</th>
+                                <td>1 cup (114 g)	</td>
+                                <td>71 cal</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div class="blog-details-pic">
+                        <div class="blog-details-pic-item">
+                            <img src="img/blog/details/details-1.jpg" alt="">
+                        </div>
+                        <div class="blog-details-pic-item">
+                            <img src="img/blog/details/details-2.jpg" alt="">
+                        </div>
+                    </div>
+                    <!-- <div class="blog-details-desc">
+                            <p>Dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div class="blog-details-quote">
+                            <div class="quote-icon">
+                                <img src="img/blog/details/quote-left.png" alt="">
+                            </div>
+                            <h5>The whole family of tiny legumes, whether red, green, yellow, or black, offers so many
+                                possibilities to create an exciting lunch.</h5>
+                            <span>MEIKE PETERS</span>
+                        </div>
+                        <div class="blog-details-more-desc">
+                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                                in. . Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+                            <p>laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit
+                                amet, consectetur adipisicing elit, sed eiusmod tempor incididunt laboris nisi ut
+                                aliquip commodo consequat. Class aptent taciti sociosqu ad litora torquent per conubia
+                                nostra, per inceptos himenaeos. Mauris vel magna ex. Integer gravida tincidunt accumsan.
+                                Vestibulum nulla mauris, condimentum id felis ac, volutpat volutpat mi qui dolorem.</p>
+                        </div> -->
+                    <!-- <div class="blog-details-tag-share">
+                            <div class="tags">
+                                <a href="#">Body buiding</a>
+                                <a href="#">Yoga</a>
+                                <a href="#">Weightloss</a>
+                                <a href="#">Streching</a>
+                            </div>
+                            <div class="share">
+                                <span>Share</span>
+                                <a href="#"><i class="fa fa-facebook"></i> 82</a>
+                                <a href="#"><i class="fa fa-twitter"></i> 24</a>
+                                <a href="#"><i class="fa fa-envelope"></i> 08</a>
+                            </div>
+                        </div>
+                        <div class="blog-details-author">
+                            <div class="ba-pic">
+                                <img src="img/blog/details/blog-profile.jpg" alt="">
+                            </div>
+                            <div class="ba-text">
+                                <h5>Lena Mollein.</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation.</p>
+                                <div class="bp-social">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                    <a href="#"><i class="fa fa-youtube-play"></i></a>
+                                </div>
+                            </div>
+                        </div> -->
+                    <!-- <div class="row">
+                            <div class="col-lg-6">
+                                <div class="comment-option">
+                                    <h5 class="co-title">Comment</h5>
+                                    <div class="co-item">
+                                        <div class="co-widget">
+                                            <a href="#"><i class="fa fa-heart-o"></i></a>
+                                            <a href="#"><i class="fa fa-share-square-o"></i></a>
+                                        </div>
+                                        <div class="co-pic">
+                                            <img src="img/blog/details/comment-1.jpg" alt="">
+                                            <h5>Brandon Kelley</h5>
+                                        </div>
+                                        <div class="co-text">
+                                            <p>Neque porro quisquam est, qui dolorem ipsum dolor sit amet, consectetur,
+                                                adipisci velit dolore.</p>
+                                        </div>
+                                    </div>
+                                    <div class="co-item reply-comment">
+                                        <div class="co-widget">
+                                            <a href="#"><i class="fa fa-heart-o"></i></a>
+                                            <a href="#"><i class="fa fa-share-square-o"></i></a>
+                                        </div>
+                                        <div class="co-pic">
+                                            <img src="img/blog/details/comment-2.jpg" alt="">
+                                            <h5>Brandon Kelley</h5>
+                                        </div>
+                                        <div class="co-text">
+                                            <p>Neque porro quisquam est, qui dolorem ipsum dolor sit amet, consectetur,
+                                                adipisci velit dolore.</p>
+                                        </div>
+                                    </div>
+                                    <div class="co-item">
+                                        <div class="co-widget">
+                                            <a href="#"><i class="fa fa-heart-o"></i></a>
+                                            <a href="#"><i class="fa fa-share-square-o"></i></a>
+                                        </div>
+                                        <div class="co-pic">
+                                            <img src="img/blog/details/comment-3.jpg" alt="">
+                                            <h5>Brandon Kelley</h5>
+                                        </div>
+                                        <div class="co-text">
+                                            <p>Neque porro quisquam est, qui dolorem ipsum dolor sit amet, consectetur,
+                                                adipisci velit dolore.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="leave-comment">
+                                    <h5>Leave a comment</h5>
+                                    <form action="#">
+                                        <input type="text" placeholder="Name">
+                                        <input type="text" placeholder="Email">
+                                        <input type="text" placeholder="Website">
+                                        <textarea placeholder="Comment"></textarea>
+                                        <button type="submit">Submit</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Blog Details Section End -->
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\BlogController;
+use App\Http\Controllers\Frontend\ContactController;
+use App\Http\Controllers\Frontend\AboutController;
+use App\Http\Controllers\Frontend\BMIController;
+use App\Http\Controllers\Frontend\DietController;
+use App\Http\Controllers\Frontend\TrainingController;
+use App\Http\Controllers\Frontend\GalleryController;
+use App\Http\Controllers\Frontend\BlogDetailController;
+use App\Http\Controllers\Frontend\DietDetailController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', [\App\Http\Controllers\Frontend\HomeController::class,'index']);
+// Route::get('/', 'FrontendController@index');
+// Route::get('/about', [\App\Http\Controllers\Frontend\AboutController::class,'index']);
+
+//
+// Route::get('/blog', [::class,'index']);
+
+Route::resource('/', HomeController::class);
+Route::resource('blog', BlogController::class);
+Route::resource('contact', ContactController::class);
+Route::resource('about', AboutController::class);
+Route::resource('bmi', BMIController::class);
+Route::resource('diet', DietController::class);
+Route::resource('training', TrainingController::class);
+Route::resource('gallery', GalleryController::class);
+Route::resource('blog_detail', BlogDetailController::class);
+Route::resource('diet_detail', DietDetailController::class);
+
+// Route::resource('blog', BlogController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
