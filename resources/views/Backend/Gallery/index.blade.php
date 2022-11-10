a34d15e75f642bacad2ef6e502f22aebf8500658
sithumyo87 <sithumyo87@gmail.com>
2022-11-10 16:59:10 +0630

    Table Add

--- resources/views/Backend/Gallery/index.blade.php
+++ resources/views/Backend/Gallery/index.blade.php
@@ -1,23 +1,21 @@
 @extends('Backend.master')
 @section('title','Gallery')
 @section('content')
-<div class="col-md-6 mt-5 ">
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-               <h1>Helo World</h1>
-              </div>
+<div class="col-md-12 mt-5 ">
+        <table class="table table-bordered">
+            <tr>
+                <th>Id</th>
+                <th>Image</th>
+                <th colspan="2">Action</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td> <img src="https://www.shutterstock.com/image-photo/wall-decor-interior-home-decoration-600w-2019199433.jpg" alt="" width="200" height="300" > </td>
+                <td><i class="mdi mdi-square-edit-outline">
+                    
+                </i></td>
+                <td></td>
+            </tr>
+        </table>
+ </div>
 @endsection
